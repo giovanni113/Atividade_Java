8e6b5578947542ca06e78ffafef7020e10f6192c
giovanni113 <giovanni.diniz@etec.sp.gov.br>
2019-05-27 22:58:11 -0300

    Fix e Upgrade

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class ClassePrincipal {
@@ -6,7 +5,6 @@ public class ClassePrincipal {
     public static void main(String[] args) {
 
         int option;
-
         ContaBancaria contaBancaria = new ContaBancaria();
         Scanner scan = new Scanner(System.in);
 
@@ -19,21 +17,54 @@ public class ClassePrincipal {
 
             switch (option) {
                 case 1:
+                    System.out.println("Digite seu CPF: ");
+                    contaBancaria.cadastrocpf = (scan.nextDouble());
                     System.out.println("Digite sua nova senha: ");
-                    contaBancaria.cadastro = scan.next();
+                    contaBancaria.cadastrosenha = (scan.nextDouble());
                     break;
                 case 2:
+                    System.out.println("Digite seu CPF: ");
+                    contaBancaria.cpf = (scan.nextDouble());
                     System.out.println("Digite sua senha: ");
-                    contaBancaria.comparar(scan.nextDouble());
-                    break;
-                        
+                    contaBancaria.senha = (scan.nextDouble());
+
+                    if (contaBancaria.cadastrosenha.equals(contaBancaria.senha) && contaBancaria.cadastrocpf.equals(contaBancaria.cpf)) {
+
+                        do {
+                            System.out.println("Digite sua senha:");
+                            System.out.println("=== 1- Saque ===");
+                            System.out.println("=== 2- Depósito ===");
+                            System.out.println("=== 3- Verificar Saldo ===");
+                            System.out.println("=== 4- Encerrar ===");
+                            System.out.println(" Opção: ");
+                            option = scan.nextInt();
+
+                            switch (option) {
+                                case 1:
+                                    contaBancaria = new ContaBancaria();
+                                    System.out.println("Valor a ser sacado:");
+                                    contaBancaria.sacar(scan.nextDouble());
+                                    break;
+                                case 2:
+                                    contaBancaria = new ContaBancaria();
+                                    System.out.println("Valor a ser depositado:");
+                                    contaBancaria.depositar(scan.nextDouble());
+                                    break;
+                                case 3:
+                                    System.out.println(contaBancaria.saldo);
+                                case 4:
+                                    System.exit(0);
+                                    break;
+                                default:
+                                    System.out.println("Opção inválida.");
+                            }
+                        } while (option != 0);
+                    }
+
             }
-                    System.out.println("Programa encerrado.");
-            
-            
+
         } while (option != 0);
         System.out.println("Programa encerrado.");
-    
 //        ContaBancaria contaBancaria = new ContaBancaria();
 //        
 //        System.out.println("Digite seu Nome:");
--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,51 +1,18 @@
-
 import java.util.Scanner;
 
 public class ContaBancaria {
-    
-    ContaBancaria contaBancaria = new ContaBancaria();
 
-    String cadastro;
-    String senha;
-    String nomeDoCliente;
+
+    Double cadastrocpf;
+    Double cpf;
+    Double cadastrosenha;
+    Double senha;
+    Double nomeDoCliente;
     int numero;
     double saldo = 500.00;
     int option;
 
     Scanner scan = new Scanner(System.in);
-
-    public void comparar(double valorasercomparado) {
-
-        if (cadastro.equals(valorasercomparado)) {
-
-            do {
-                System.out.println("Digite sua senha:");
-                System.out.println("=== 1- Saque ===");
-                System.out.println("=== 2- Depósito ===");
-                System.out.println("=== 3- Encerrar ===");
-                System.out.println(" Opção: ");
-                option = scan.nextInt();
-
-                switch (option) {
-                    case 1:
-                        contaBancaria = new ContaBancaria();
-                        System.out.println("Valor a ser sacado:");
-                        contaBancaria.sacar(scan.nextDouble());
-                        break;
-                    case 2:
-                        contaBancaria = new ContaBancaria();
-                        System.out.println("Valor a ser depositado:");
-                        contaBancaria.depositar(scan.nextDouble());
-                        break;
-                    case 3:
-                        System.exit(0);
-                        break;
-                    default:
-                        System.out.println("Opção inválida.");
-                }
-            } while (option != 0);
-        }
-    }
 
     public void sacar(double valorasersacado) {
 
